504f41728b303518bf761f86bba5674f10df2aeb
mfdemirci <stevybeatz@gmail.com>
2025-06-19 14:55:49 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import HeaderPreview from './components/HeaderPreview';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeaderPreview />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderPreview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderPreview = () => {
+  return (
+    <div style={{ width: "100%" }}>
+      <img src="/figma-header.png" alt="Figma Header" style={{ width: "100vw", display: "block", marginTop: "20px" }} />
+    </div>
+  );
+};
+
+export default HeaderPreview;
