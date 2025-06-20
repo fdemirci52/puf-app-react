71010af7795a8fb174d80e8cc2b12e572bc8fd17
mfdemirci <stevybeatz@gmail.com>
2025-06-20 17:42:57 +0300

    Commit3

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Header from './components/Header';
 import HeaderPreview from './components/HeaderPreview';
 import Header2 from './components/Header2'; 
 import Rotate from './components/Rotate';
+import WorldAppTop from './components/WorldAppTop';
 
 function App() {
   return (
     <div>
+      <WorldAppTop />
       <Header />
       <Header2 />
       <HeaderPreview />
